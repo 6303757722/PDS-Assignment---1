03f182ddeae0629b43b193ea4be694b013155c3b
Kasam Sai Vardhan <43110988+6303757722@users.noreply.github.com>
2024-02-12 23:44:15 -0600

    Add files via upload

---
+++ PDS_UMKC/PDS/Assignment/1.1/src/loading_frailtydata.R
@@ -0,0 +1,6 @@
+#Reading the csv file
+frailty = read.csv("C:/Users/vardhank/OneDrive/Desktop/PDS_UMKC/PDS/Assignment/1.1/data_raw/raw_frailty_data1.csv")
+frailty
+
+#Getting the info of complete csv
+summary(frailty)
---
+++ PDS_UMKC/PDS/Assignment/1.2/src/loading_StudentsData.R
@@ -0,0 +1,6 @@
+#Reading the csv file
+Student = read.csv("C:/Users/vardhank/OneDrive/Desktop/PDS_UMKC/PDS/Assignment/1.2/data_raw/Raw_StudentsPerformance.csv")
+Student
+
+#Getting the info of complete csv
+summary(Student)
